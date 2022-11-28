db7b7f5333ecec11d9614d36cfc3f8496aaef248
root <root@MisakaNode2.localdomain>
2022-11-28 17:42:10 -0500

    add module 10 6341

---
+++ CSCI6341/Module10/works/BracketSearch.java
@@ -0,0 +1,39 @@
+
+public class BracketSearch {
+
+
+    public static void main (String[] argv)
+    {
+        bracketSearch (0, 10);
+    }
+    
+
+    static void bracketSearch (double a, double b)
+    {
+        int M = 6;
+        int N = 4;
+        double bestx = a;
+        double bestf = computef (bestx);
+        for (int i=0; i<N; i++) {
+            double delta = (b-a) / M;
+            for (double x=a+delta; x<=b; x+=delta) {
+                double f = computef (x);
+                if (f < bestf) {
+                    bestf = f;
+                    bestx = x;
+                }
+            }
+            a = bestx - delta;
+            b = bestx + delta;
+        }
+        System.out.println ("a=" + a + " b=" + b + " bestf=" + bestf);
+    }
+
+
+    static double computef (double x) 
+    {
+        double f = 2.5 + (x - 4.71) * (x - 4.71);
+        return f;
+    }
+
+}
